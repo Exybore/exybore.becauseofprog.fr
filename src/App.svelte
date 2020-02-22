cfd863313aed5126c9cb6b220ef8d32645ad963c
Exybore <exybore@becauseofprog.fr>
2020-02-22 19:30:59 +0100

    style: update profile picture

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
-  import Link from './Link.svelte'
+  import Link from "./Link.svelte";
 
   let links = [
     {
-      name: 'BecauseOfProg team',
-      id: 'bop',
-      href: 'https://becauseofprog.fr'
+      name: "BecauseOfProg team",
+      id: "bop",
+      href: "https://becauseofprog.fr"
     },
     {
       name: "Keybase",
@@ -31,17 +31,17 @@
       name: "Twitter",
       id: "twitter",
       href: "https://twitter.com/exybore"
-    },{
+    },
+    {
       name: "Youtube channel",
       id: "youtube",
       href: "https://www.youtube.com/channel/UC9Q3XhnYp-bZUgE995Csxww"
-    },
-
-  ]
+    }
+  ];
 </script>
 
 {#each links as link}
   <Link {...link} />
 {/each}
 
-<img id="avatar" src="img/avatar.jpg" alt="Profile picture"/>
+<img id="avatar" src="img/avatar.png" alt="Profile picture" />
